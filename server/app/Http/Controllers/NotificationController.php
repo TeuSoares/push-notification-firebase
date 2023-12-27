ace96af888543054acd65788e396b86557ba1bd8
TeuSoares <mateussoares114@gmail.com>
2023-12-27 15:16:47 -0300

    Send notification to multiples devices

--- server/app/Http/Controllers/NotificationController.php
+++ server/app/Http/Controllers/NotificationController.php
@@ -9,29 +9,36 @@ class NotificationController extends Controller
 {
     public function sendNotification(Request $request)
     {
+        $deviceTokens = [
+            'f1Hi0lcTN9mmCu1RWy7i_L:APA91bHjd8V-Buh3JpNzjES-azomupOWk4BY8rqhg01kQoycz2oNIbgmOhtg-iOTpSxy15Hl8UVzGopyJS9UZIn5QCFr53amLa4LFj5GhLGXpkKeeKWlS9iW-BblskRRnpmjB8PHErUk',
+            'c-HjmHSI56PCo4qZgykeWf:APA91bHzBklPR1aX867pgTwZiEGH6sXhojmQPDNPLo89Qr1O3eb1FvopZhDePtTfDI1B44Lf9KXmqCKf-RiyHyVKdOtQsZXl2BrUBeMeb22HNi2hERyEwGvgfV5Y08GMAPIysR4niX9M'
+        ];
+
         $headers = [
             'Authorization' => 'Bearer ' . $this->getGoogleAccessToken(),
             'Content-Type' => 'application/json'
         ];
 
-        $data = [
-            "message" => [
-                "token" => env('FIREBASE_CLIENT_TOKEN'),
-                "notification" => [
-                  "title" => $request->title,
-                  "body" => $request->body
+        $messages_ids = [];
+
+        foreach ($deviceTokens as $token) {
+            $data = [
+                "message" => [
+                    "token" => $token,
+                    "notification" => [
+                      "title" => $request->title,
+                      "body" => $request->body
+                    ]
                 ]
-            ]
-        ];
+            ];
 
-        $response = Http::withHeaders($headers)
-            ->post('https://fcm.googleapis.com/v1/projects/push-notification-2cb1f/messages:send', $data);
+            $response = Http::withHeaders($headers)
+                ->post('https://fcm.googleapis.com/v1/projects/push-notification-2cb1f/messages:send', $data);
 
-        if ($response->failed()) {
-            return $response->throw();
+            array_push($messages_ids, $response->json());
         }
 
-        return $response->json();
+        return response()->json($messages_ids);
     }
 
     private function getGoogleAccessToken() {
